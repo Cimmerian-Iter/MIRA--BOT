a58c8c44526831ad0fff4c0589b57ed8a6ba5d54
Samilop Iter <37327589+Cimmerian-Iter@users.noreply.github.com>
2018-11-29 04:46:51 -0800

    Capteur de distance

---
+++ test_distance.ino
@@ -0,0 +1,24 @@
+const int echoPin = 12;  // Echo    (réception)
+ 
+// Variables utiles
+long duree;   // durée de l'echo
+int distance; // distance
+ 
+ 
+void setup() {
+pinMode(echoPin, INPUT);  // Configuration du port de l'Echo  comme une ENTREE
+ 
+Serial.begin(9600); // Démarrage de la communication série
+}
+ 
+void loop() {
+  duree = pulseIn(echoPin, HIGH);
+ 
+  // Calcul de la distance
+  distance = duree/147*3;
+ 
+ // Affichage de la distance dans le Moniteur Série
+ Serial.print("Distance : ");
+ Serial.print(distance);
+ Serial.println("cm");
+}
